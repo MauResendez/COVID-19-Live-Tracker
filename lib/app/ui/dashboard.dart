13296b253ef7c23bf7d37e31900fe4e78f8915b1
Mauricio Resendez <Mauricio.resendez01@utrgv.edu>
2020-06-27 04:54:37 -0500

    Shows dlalog alert for internet and server issues

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -3,8 +3,10 @@ import 'package:covid19_live_tracker/app/repositories/endpoints_data.dart';
 import 'package:covid19_live_tracker/app/services/api.dart';
 import 'package:covid19_live_tracker/app/ui/endpoint_card.dart';
 import 'package:covid19_live_tracker/app/ui/last_updated_status_text.dart';
+import 'package:covid19_live_tracker/app/ui/show_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:io';
 
 class Dashboard extends StatefulWidget
 {
@@ -24,9 +26,20 @@ class _DashboardState extends State<Dashboard>
 
   Future<void> _updateData() async
   {
-    final dataRepository = Provider.of<DataRepository>(context, listen: false); // Have a data repository object ready to be able to retrieve data from it, depending on endpoint
-    final endpointsData = await dataRepository.getAllEndpointsData();
-    setState(() => _endpointsData = endpointsData); // Updates number when refreshed
+    try
+    {
+      final dataRepository = Provider.of<DataRepository>(context, listen: false); // Have a data repository object ready to be able to retrieve data from it, depending on endpoint
+      final endpointsData = await dataRepository.getAllEndpointsData();
+      setState(() => _endpointsData = endpointsData); // Updates number when refreshed
+    }
+    on SocketException catch (_) // When there's no internet connection to use to get the data, show this dialog alert
+    {
+      showAlertDialog(context: context, title: 'Connection Error', content: 'Could not retrieve data. Please try again later.', defaultActionText: 'OK');
+    }
+    catch (_) // When something is wrong with the API Server, show this dialog alert
+    {
+      showAlertDialog(context: context, title: 'Server Error', content: 'Please contact support or try again later.', defaultActionText: 'OK');
+    }
   }
 
   Widget build(BuildContext context)
